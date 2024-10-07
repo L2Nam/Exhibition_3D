6d393b364e81537945cca4e6c0292bd6a4370b84
L2Nam <lenamlhn234@gmail.com>
2024-10-08 03:24:16 +0700

    glass room

---
+++ Assets/Scripts/SkyboxChanger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxChanger : MonoBehaviour
+{
+    public List<Material> skyboxMaterials;
+
+    private int currentSkyboxIndex = 0;
+
+    void Start()
+    {
+        if (skyboxMaterials.Count > 0)
+        {
+            RenderSettings.skybox = skyboxMaterials[currentSkyboxIndex];
+            InvokeRepeating("ChangeSkybox", 18f, 18f);
+        }
+        else
+        {
+            Debug.LogError("No skybox materials assigned!");
+        }
+    }
+
+    public void ChangeSkybox()
+    {
+        currentSkyboxIndex = (currentSkyboxIndex + 1) % skyboxMaterials.Count;
+        RenderSettings.skybox = skyboxMaterials[currentSkyboxIndex];
+        DynamicGI.UpdateEnvironment();
+    }
+}
